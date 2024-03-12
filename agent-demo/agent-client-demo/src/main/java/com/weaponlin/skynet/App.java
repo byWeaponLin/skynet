58a687476e5cf129e428e44163959fa09747e1e5
Weapon Lin <weaponlin@126.com>
2024-03-12 23:22:24 +0800

    third commit

--- agent-demo/agent-client-demo/src/main/java/com/weaponlin/skynet/App.java
+++ agent-demo/agent-client-demo/src/main/java/com/weaponlin/skynet/App.java
@@ -1,13 +1,28 @@
 package com.weaponlin.skynet;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpResponse response = null;
+        try {
+            //1.获得一个httpclient对象
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            //2.生成一个get请求
+            HttpGet httpget = new HttpGet("http://console.weiyun.baidu.com/");
+            //3.执行get请求并返回结果
+            response = httpclient.execute(httpget);
+            System.out.println(response.toString());
+            //4.处理结果
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            response.close();
+        }
     }
 }
